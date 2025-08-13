0f6d65cf59a804cbdb0907b4e256c158b2db903c
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 12:43:12 +0530

    Update main.tf

---
+++ infra/terraform/modules/database/main.tf
@@ -0,0 +1,27 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = var.private_subnet_ids
+
+  tags = {
+    Name    = "${var.project_name}-db-subnet-group"
+    Project = var.project_name
+  }
+}
+
+resource "aws_db_instance" "app_db" {
+  identifier        = "${var.project_name}-db"
+  engine            = "mysql"
+  instance_class    = var.db_instance_class
+  allocated_storage = var.allocated_storage
+  name              = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
+  publicly_accessible  = false
+  skip_final_snapshot   = true
+
+  tags = {
+    Name    = "${var.project_name}-db"
+    Project = var.project_name
+  }
+}
