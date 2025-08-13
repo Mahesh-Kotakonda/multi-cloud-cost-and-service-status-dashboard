6878b7ca63a0efae43e88c5853ac3c95f6c09946
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 14:45:57 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -60,6 +60,5 @@ module "database" {
   db_username      = var.db_username
   db_password      = var.db_password
   subnet_ids       = module.vpc.private_subnet_ids
-  security_group_id = module.security.db_sg_id
 }
 
