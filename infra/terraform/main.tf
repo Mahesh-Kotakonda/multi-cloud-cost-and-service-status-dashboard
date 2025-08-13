8531060bba0a7832d97b8dc39d3be9f623af65d0
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 14:42:16 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -47,8 +47,8 @@ module "alb" {
   project_name      = var.project_name
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
-  # security_group_id = module.security.alb_sg_id
-  # target_instance_ids = module.ec2.instance_ids
+  security_group_id = module.security.alb_sg_id
+  target_instance_ids = module.ec2.instance_ids
 }
 
 # Call Database module
