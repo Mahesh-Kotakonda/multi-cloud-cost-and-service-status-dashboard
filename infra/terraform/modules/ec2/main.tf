3a0eedb7665c2b93e43164ccd4e900d9b98b37a4
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 16:51:06 +0530

    Update main.tf

--- infra/terraform/modules/ec2/main.tf
+++ infra/terraform/modules/ec2/main.tf
@@ -1,13 +1,17 @@
+
 resource "aws_instance" "app" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
-  public_subnet_ids     = var.subnet_ids[count.index]
+  subnet_id     = var.public_subnet_ids[count.index]
   key_name      = var.key_name
+
+  # This is the key line. vpc_security_group_ids requires the VPC ID indirectly.
+  # The security group you create in the security module is already tied to the VPC.
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name    = "${var.project_name}-app-${count.index + 1}"
     Project = var.project_name
   }
 }
-
