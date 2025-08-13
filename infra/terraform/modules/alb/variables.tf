dbc7f435f672154df8fb880d08c37b64caf4ec2e
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 12:41:41 +0530

    Update variables.tf

---
+++ infra/terraform/modules/alb/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where ALB will be deployed"
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "Public subnet IDs for ALB"
+}
