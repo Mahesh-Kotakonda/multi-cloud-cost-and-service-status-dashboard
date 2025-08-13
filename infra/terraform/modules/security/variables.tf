44499597be89d433caba2c49bf6a80c3b6a68c72
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 12:58:04 +0530

    Update variables.tf

---
+++ infra/terraform/modules/security/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+  type        = string
+  description = "Project name for tagging"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where security groups will be created"
+}
