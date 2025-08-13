fd67e918c1309af6febb1b59f49e981e205bc8e2
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 17:02:35 +0530

    Update main.tf

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -1,36 +1,12 @@
-# Security group for ALB
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.project_name}-alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name    = "${var.project_name}-alb-sg"
-    Project = var.project_name
-  }
-}
+# In ./modules/alb/main.tf
 
 # ALB
 resource "aws_lb" "app_alb" {
   name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_group_id    = [aws_security_group.alb_sg.id]
-  public_subnet_ids            = var.public_subnet_ids
+  security_groups    = [var.security_group_id]
+  subnets            = var.public_subnet_ids
 
   tags = {
     Name    = "${var.project_name}-alb"
@@ -40,10 +16,10 @@ resource "aws_lb" "app_alb" {
 
 # Target Group
 resource "aws_lb_target_group" "app_tg" {
-  name     = "${var.project_name}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
 
   health_check {
     path                = "/"
@@ -72,3 +48,11 @@ resource "aws_lb_listener" "app_listener" {
   }
 }
 
+# Target Group Attachment
+# This resource attaches the EC2 instances to the Target Group
+resource "aws_lb_target_group_attachment" "app_attach" {
+  count            = length(var.target_instance_ids)
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = var.target_instance_ids[count.index]
+  port             = 80
+}
