0be87e22a12746243d44fff350d6810d1cbf477f
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 17:07:55 +0530

    Update main.tf

--- infra/terraform/modules/database/main.tf
+++ infra/terraform/modules/database/main.tf
@@ -9,20 +9,22 @@ resource "aws_db_subnet_group" "db_subnets" {
 }
 
 resource "aws_db_instance" "app_db" {
-  identifier        = "${var.project_name}-db"
-  engine            = "mysql"
-  instance_class    = var.db_instance_class
-  allocated_storage = var.allocated_storage
-  name              = var.db_name
-  username          = var.db_username
-  password          = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.db_subnets.name
-  publicly_accessible  = false
-  skip_final_snapshot   = true
+  identifier               = "${var.project_name}-db"
+  engine                   = "mysql"
+  instance_class           = var.db_instance_class
+  allocated_storage        = var.allocated_storage
+  # This is the correct argument for the database name within the instance
+  name                     = var.db_name 
+  username                 = var.db_username
+  password                 = var.db_password
+  db_subnet_group_name     = aws_db_subnet_group.db_subnets.name
+  publicly_accessible      = false
+  skip_final_snapshot      = true
 
   tags = {
     Name    = "${var.project_name}-db"
     Project = var.project_name
   }
 }
+
 
