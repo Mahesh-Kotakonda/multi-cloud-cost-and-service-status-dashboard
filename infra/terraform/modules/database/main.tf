a5b008e6301bb31ee0fb70e2bfc36e6a3716aed6
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 17:17:36 +0530

    Update main.tf

--- infra/terraform/modules/database/main.tf
+++ infra/terraform/modules/database/main.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "app_db" {
   instance_class           = var.db_instance_class
   allocated_storage        = var.allocated_storage
   # This is the correct argument for the database name within the instance
-  name                     = var.db_name 
+  db_instance_identifier                  = var.db_name 
   username                 = var.db_username
   password                 = var.db_password
   db_subnet_group_name     = aws_db_subnet_group.db_subnets.name
@@ -26,5 +26,6 @@ resource "aws_db_instance" "app_db" {
     Project = var.project_name
   }
 }
+
 
 
