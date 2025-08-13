eb87667b7e7dbce47993469e032577701e8edf63
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-13 17:41:51 +0530

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -6,8 +6,7 @@ variable "aws_region" {
 variable "instance_count" {
   description = "The number of EC2 instances to create."
   type        = number
-
-  # default     = 1
+  default     = 1
 }
 
 variable "project_name" {
