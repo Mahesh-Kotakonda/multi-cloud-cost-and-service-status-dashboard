31f545faee74022016d804827fc8c12c5e46873f
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-16 18:57:59 +0530

    Update database.py

--- app/backend/core/database.py
+++ app/backend/core/database.py
@@ -1,23 +1,24 @@
 # backend/core/database.py
 import os
-import psycopg2
+import pymysql
 
 def get_db_connection():
-    """Create DB connection using environment variables."""
-    host = os.getenv("DB_HOST")       # REQUIRED
-    port = int(os.getenv("DB_PORT", 5432))
-    dbname = os.getenv("DB_NAME")     # REQUIRED
-    user = os.getenv("DB_USER")       # REQUIRED
-    password = os.getenv("DB_PASS")  # REQUIRED
+    """Create DB connection using env vars (MySQL)."""
+    host = os.environ.get("DB_HOST")
+    port = int(os.environ.get("DB_PORT", 3306))   # MySQL default
+    dbname = os.environ.get("DB_NAME")
+    user = os.environ.get("DB_USER")
+    password = os.environ.get("DB_PASS")
 
     if not all([host, dbname, user, password]):
         raise ValueError("DB_HOST, DB_NAME, DB_USER, and DB_PASSWORD must be set")
 
-    conn = psycopg2.connect(
+    conn = pymysql.connect(
         host=host,
         port=port,
-        dbname=dbname,
+        db=dbname,
         user=user,
-        password=password
+        password=password,
+        cursorclass=pymysql.cursors.DictCursor
     )
     return conn
