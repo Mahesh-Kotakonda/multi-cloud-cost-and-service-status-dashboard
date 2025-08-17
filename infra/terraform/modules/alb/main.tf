7ecfd1b2215d717c21cd42f5150a8f03dabcb8f6
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-17 18:33:45 +0530

    Update main.tf

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -111,25 +111,5 @@ resource "aws_lb_target_group_attachment" "backend_attach" {
   port             = 8080
 }
 
-#########################################
-# Outputs
-#########################################
-output "alb_arn" {
-  value       = aws_lb.app_alb.arn
-  description = "ARN of the Application Load Balancer"
-}
 
-output "alb_dns" {
-  value       = aws_lb.app_alb.dns_name
-  description = "DNS name of the Application Load Balancer"
-}
 
-output "frontend_target_group_arn" {
-  value       = aws_lb_target_group.frontend_tg.arn
-  description = "ARN of the frontend target group (port 3000)"
-}
-
-output "backend_target_group_arn" {
-  value       = aws_lb_target_group.backend_tg.arn
-  description = "ARN of the backend target group (port 8080)"
-}
