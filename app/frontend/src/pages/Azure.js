e9f17f1735282dff7d7cc5a3e4244da1702e4ce7
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-18 08:16:49 +0530

    Update Azure.js

---
+++ app/frontend/src/pages/Azure.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Azure() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Azure Dashboard</h2>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
+
+export default Azure;
