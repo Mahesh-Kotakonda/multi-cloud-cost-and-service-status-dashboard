df4a6c737707b154fcc14e5f0633bc3468ce7b91
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-18 08:16:33 +0530

    Update GCP.js

---
+++ app/frontend/src/pages/GCP.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GCP() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>GCP Dashboard</h2>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
+
+export default GCP;
