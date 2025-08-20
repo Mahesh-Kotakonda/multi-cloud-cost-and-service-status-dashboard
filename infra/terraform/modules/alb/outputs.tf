9b80017d4956ee4347c99188730fb64558f383ad
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-20 08:17:41 +0530

    Update outputs.tf

--- infra/terraform/modules/alb/outputs.tf
+++ infra/terraform/modules/alb/outputs.tf
@@ -11,15 +11,31 @@ output "alb_dns" {
   description = "DNS name of the Application Load Balancer"
 }
 
+
 # ----------------------------
-# Target Group Outputs
+# Frontend Target Groups
 # ----------------------------
-output "frontend_target_group_arn" {
-  value       = aws_lb_target_group.frontend_tg.arn
-  description = "ARN of the frontend target group (port 3000)"
+output "frontend_blue_tg_arn" {
+  value       = aws_lb_target_group.frontend_blue_tg.arn
+  description = "ARN of the frontend blue target group (port 3000)"
 }
 
-output "backend_target_group_arn" {
-  value       = aws_lb_target_group.backend_tg.arn
-  description = "ARN of the backend target group (port 8080)"
+output "frontend_green_tg_arn" {
+  value       = aws_lb_target_group.frontend_green_tg.arn
+  description = "ARN of the frontend green target group (port 3001)"
 }
+
+# ----------------------------
+# Backend Target Groups
+# ----------------------------
+output "backend_blue_tg_arn" {
+  value       = aws_lb_target_group.backend_blue_tg.arn
+  description = "ARN of the backend blue target group (port 8080)"
+}
+
+output "backend_green_tg_arn" {
+  value       = aws_lb_target_group.backend_green_tg.arn
+  description = "ARN of the backend green target group (port 8081)"
+}
+
+
