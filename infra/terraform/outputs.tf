bbff2b70fc3f25f3fac3f5434a08a20aa0be1181
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-20 08:21:37 +0530

    Update outputs.tf

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -5,31 +5,26 @@ output "project_name" {
 
 
 
-# ----------------------------
-# Frontend Target Groups
-# ----------------------------
 output "frontend_blue_tg_arn" {
-  value       = aws_lb_target_group.frontend_blue_tg.arn
+  value       = module.alb.frontend_blue_tg_arn
   description = "ARN of the frontend blue target group (port 3000)"
 }
 
 output "frontend_green_tg_arn" {
-  value       = aws_lb_target_group.frontend_green_tg.arn
+  value       = module.alb.frontend_green_tg_arn
   description = "ARN of the frontend green target group (port 3001)"
 }
 
-# ----------------------------
-# Backend Target Groups
-# ----------------------------
 output "backend_blue_tg_arn" {
-  value       = aws_lb_target_group.backend_blue_tg.arn
+  value       = module.alb.backend_blue_tg_arn
   description = "ARN of the backend blue target group (port 8080)"
 }
 
 output "backend_green_tg_arn" {
-  value       = aws_lb_target_group.backend_green_tg.arn
+  value       = module.alb.backend_green_tg_arn
   description = "ARN of the backend green target group (port 8081)"
 }
+
 
 
 # VPC outputs
