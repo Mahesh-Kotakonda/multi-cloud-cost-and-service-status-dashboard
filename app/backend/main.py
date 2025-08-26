15e1a5c5fcc53ad42980b8ecc5bbd6c820da1998
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-26 08:31:29 +0530

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("APP_PORT", 8000))
     reload = os.environ.get("APP_RELOAD", "false").lower() == "true"
 
-    print(f"\n🚀 Starting FastAPI server at: http://{host}:{port}")
+    print(f"\n Starting FastAPI server at: http://{host}:{port}")
     print(f"📖 Swagger docs available at: http://{host}:{port}/docs")
     print(f"ReDoc docs available at:   http://{host}:{port}/redoc")
     print(f"AWS Costs endpoint:        http://{host}:{port}/aws/costs")
