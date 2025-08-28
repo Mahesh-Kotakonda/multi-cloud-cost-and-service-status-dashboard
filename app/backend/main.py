a2790241d271fb69cf353d26a7f5ce1ac7dd25a3
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-08-28 07:17:21 +0530

    Update main.py

--- app/backend/main.py
+++ app/backend/main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     print(f"📖 Swagger docs available at: http://{host}:{port}/docs")
     print(f"ReDoc docs available at:   http://{host}:{port}/redoc")
     print(f"AWS Costs endpoint:        http://{host}:{port}/aws/costs")
-    print(f"🖥EC2 Status endpoint:      http://{host}:{port}/aws/ec2-status\n")
+    print(f"EC2 Status endpoint:      http://{host}:{port}/aws/ec2-status\n")
 
     uvicorn.run(
         "api.metrics:app",
