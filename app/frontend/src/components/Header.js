9d5df144d162dddd579fab88c2cb51b34b7d2ab5
Mahesh Kotakonda <mahesh04815@gmail.com>
2025-09-24 18:14:00 +0530

    Update Header.js

--- app/frontend/src/components/Header.js
+++ app/frontend/src/components/Header.js
@@ -5,26 +5,15 @@ function Header() {
   return (
     <header className="header-container">
       <div className="header-content">
-        <h1 className="header-title">Multi-Cloud Monitoring Dashboard</h1>
-        <p className="header-subtitle">
-          Real-time updates on cloud costs and service status across AWS, GCP,Azure and other clouds.
-        </p>
-
+        <div className="header-text">
+          <h1 className="header-title">Multi-Cloud Monitoring Dashboard</h1>
+          <p className="header-subtitle">
+            Real-time updates on cloud costs and service status across AWS, GCP, and Azure.
+          </p>
+        </div>
       </div>
     </header>
   );
 }
 
 export default Header;
-
-
-
-
-
-
-
-
-
-
-
-
